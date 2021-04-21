18259cffc4e52f768b461841c495dd42e90b267e
steinuil <steenuil.owl@gmail.com>
2021-04-22 00:20:17 +0200

    date

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell rec {
-  buildInputs = with pkgs; [ clang m4 binutils pkgconfig gmp autoconf libseccomp inotify-tools glibc ];
+  buildInputs = with pkgs; [ clang m4 binutils pkgconfig gmp autoconf libseccomp inotify-tools glibc zlib ];
 
   shellHook = ''
     eval $(opam env)
