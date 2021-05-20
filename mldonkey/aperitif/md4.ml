1a4ac1a3b8ac84aaccfa505acdf5d55d573c80ec
steinuil <steenuil.owl@gmail.com>
2021-05-20 02:00:29 +0200

    all tests

--- mldonkey/aperitif/md4.ml
+++ mldonkey/aperitif/md4.ml
@@ -134,8 +134,26 @@ let digest ?(off = 0) ?len input =
   Bytes.set_int32_le x 8 !c;
   Bytes.set_int32_le x 12 !d;
 
-  Base16.encode_bytes_exn x
+  Base16.encode_bytes_exn ~alphabet:Base16.lowercase_alphabet x
 
 let%test _ = digest "" = "31d6cfe0d16ae931b73c59d7e0c089c0"
+
+let%test _ = digest "a" = "bde52cb31de33e46245e05fbdbd6fb24"
+
+let%test _ = digest "abc" = "a448017aaf21d8525fc10ae87aa6729d"
+
+let%test _ = digest "message digest" = "d9130a8164549fe818874806e1c7014b"
+
+let%test _ =
+  digest "abcdefghijklmnopqrstuvwxyz" = "d79e1c308aa5bbcdeea8ed63df412da9"
+
+let%test _ =
+  digest "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  = "043f8582f241db351ce627e153e7f0e4"
+
+let%test _ =
+  digest
+    "12345678901234567890123456789012345678901234567890123456789012345678901234567890"
+  = "e33b4ddc9c38f2199c3e7b164fcc0536"
 
 let%test_unit _ = digest "" |> print_endline
